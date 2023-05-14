11c637930529a6359b7e5d77f8a597ba3435b5af
yoihenba <thdamyoihenba@gmail.com>
2023-05-15 02:33:15 +0530

    first push

---
+++ config.py
@@ -0,0 +1,20 @@
+# Training parameters
+
+BATCH_SIZE = 8
+EPOCHS = 30
+MOMENTUM = 0.9
+LEARNING_RATE = 0.001
+
+
+# Other configurations
+
+EXTENSION = ".jpg" #extension of the image files
+NUM_CLASS = 26 #number of output classes from the model
+ALPHABETS = ['A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X',
+            'Y', 'Z']
+MODEL_NAME = 'resnet18_CrossEntropy_SGD_'
